b219c48ca472b6a577d3a4a8cf7ac9cd5dd66118
刘庆良 <liuqingliang@liuqingliangdeMacBook-Pro.local>
2020-08-09 02:43:40 +0800

    Init: minimum

---
+++ src/test/java/com/doublefs/common/javatemplate/JavaTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doublefs.common.javatemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
